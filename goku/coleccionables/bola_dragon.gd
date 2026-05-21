efe2c5f25b48968a1bf0bd29c079adfb28417908
rocket180406 <rocketdans18@gmail.com>
2026-05-21 22:08:07 +0200

    recogida de cable

--- goku/coleccionables/bola_dragon.gd
+++ goku/coleccionables/bola_dragon.gd
@@ -1,7 +1,10 @@
 extends Area2D
 
+@onready var recoger = $recoger
+
 func _on_body_entered(body):
 	if body.is_in_group("jugadores"):
-		queue_free()
 		Global.bolas_recogidas += 1
+		recoger.play()
 		queue_free()
+		
